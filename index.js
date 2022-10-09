2d2c82db6929652d6dd19a9e8f555cf12ed942d6
MiguelRueda8 <115418140+MiguelRueda8@users.noreply.github.com>
2022-10-09 11:27:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { getConnection } = require('./db/db-connection-mongo');
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+
+getConnection();
+
+const inventario = require('./rutas/inventario');
+const usuario = require('./rutas/usuario');
+const marca = require('./rutas/marca');
+const tipoEquipo = require('./rutas/tipoEquipo');
+const estadoEquipo = require('./rutas/estadoEquipo');
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+// lectura o parsep de json
+app.use(express.json());
+
+app.use('/usuario', usuario); // http://localhost:4000/usuario GET, POST, PUT
+app.use('/marca', marca); // http://localhost:4000/usuario GET, POST, PUT
+app.use('/tipoEquipo', tipoEquipo); // http://localhost:4000/tipoEquipo GET, POST, PUT
+app.use('/inventario', inventario); // http://localhost:4000/inventario GET, POST, PUT
+app.use('/estadoEquipo', estadoEquipo); // http://localhost:4000/estadoEquipo GET, POST, PUT
+
+
+app.listen(PORT, function() {
+    console.log(`Aplicacion corriendo en el puerto  ${PORT}`);
+});
+
+
